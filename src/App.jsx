857088c59d4ba81db8f4813bd3da0004aabdfb09
mann0777 <mannamloh77@gmail.com>
2022-09-27 11:06:16 +0530

    Used useState and map function

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,24 @@ import { useState } from "react";
 
 const App=()=>{
   const[name, setName] = useState("");
-  const[email, setemail] = useState("");
+  const[email, setEmail] = useState("");
   const[address, setAddress] = useState("");
+
+  const[users, setUsers] = useState([]);
+
+  const addUser=(e)=>{
+    e.preventDefault();
+    const user={
+      name,
+      email,
+      address
+    };
+    setUsers([...users, user]);
+    setName("")
+    setEmail("")
+    setAddress("")
+
+  };
 return(
 <>
 <div className="App">
@@ -12,14 +28,14 @@ return(
       <div className ="container">
       <div className="row justify-content-center" >
       <div className="col-xs-12 col-sm-10 col-md-8 col-lg-5"> 
-      <form>
+      <form onSubmit={addUser}>
       <div className='form-group'>
         <label htmlFor="">Name</label>
         <input type="text" className= "form-control" value={name} onChange={(e)=>setName(e.target.value) }/>
       </div>
       <div className='form-group'>
         <label htmlFor="">Email</label>
-        <input type="email" className= "form-control" value={email} onChange={(e)=>setemail(e.target.value) }/>
+        <input type="email" className= "form-control" value={email} onChange={(e)=>setEmail(e.target.value) }/>
       </div>
       <div className='form-group'>
         <label htmlFor="">Address</label>
@@ -30,6 +46,48 @@ return(
       </div>
       </div>
       </div>
+
+      <table className="table table-bordered mt-5">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Edit</th>
+            <th>Delete</th>
+
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            users.map(user =>{
+              return(
+                <tr>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.address}</td>
+                <td>
+                  <button className="btn btn-info">Edit</button>
+                </td>
+                <td>
+                <button className="btn btn-danger">Delete</button>
+                </td>
+                </tr>
+                
+               
+                
+              )
+            })
+          }
+              
+            
+
+            
+          
+        </tbody>
+      </table>
+
       
     </div>
 </>
